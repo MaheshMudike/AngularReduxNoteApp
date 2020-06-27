eae3711fac4140bf38e85a2d4b1a4fe2dde0a449
Mudike Mahaesh <Emphyd12125mmah1@cogenthyd.com>
2020-06-27 22:32:04 +0530

    dcs

--- server.js
+++ server.js
@@ -3,16 +3,8 @@ const path = require ('path')
 const app = express();
 app.use(express.static(__dirname+ '/dist/automate-ang'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(_dirname+'/dist/automate-ang/src/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-//app.listen(process.env.PORT || 8080);
-
-
-// var server = app.listen(process.env.PORT || 5000), function() {
-//     var port = server.address().port;
-//     console.log("Express is working on port " + port);
-// });
-
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
