0a3e714f05e3007f30fac80e86c40d19d7b8e4ec
Mudike Mahaesh <Emphyd12125mmah1@cogenthyd.com>
2020-06-27 20:33:54 +0530

    new changes of package.json

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require ('path')
+const app = express();
+app.use(express.static(__dirname+ '/dist/automate-ang'));
+app.get('/*',function(req,res){
+    res.sendFile(path.join(_dirname+'/dist/automate-ang/index.html'));
+});
+//app.listen(process.env.PORT || 8080);
+
+
+// var server = app.listen(process.env.PORT || 5000), function() {
+//     var port = server.address().port;
+//     console.log("Express is working on port " + port);
+// });
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("Express is working on port " + port);
+  });
