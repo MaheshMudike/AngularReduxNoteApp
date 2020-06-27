9695e7cbf0b56d57b2e6960e2b7bb2dd513fedcc
Mudike Mahaesh <Emphyd12125mmah1@cogenthyd.com>
2020-06-27 22:55:04 +0530

    scd

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require ('path')
 const app = express();
-app.use(express.static(__dirname+ '/dist/automate-ang'));
+app.use(express.static(__dirname+ '/automate-ang'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/automate-ang/index.html'));
+    res.sendFile(path.join(__dirname+'/automate-ang/src/index.html'));
 });
 
 var server = app.listen(process.env.PORT || 8080, function () {
